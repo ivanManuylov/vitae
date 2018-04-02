ca31836371a6617800c603010c1fefc8ca85e69c
ivanManuylov <36048329+ivanManuylov@users.noreply.github.com>
2018-04-03 01:01:26 +0700

    Адаптив
    Получилось для 1024х768

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,13 @@
 var gulp      = require('gulp'), // Подключаем Gulp
     sass        = require('gulp-sass'), //Подключаем Sass пакет,
     browserSync = require('browser-sync'), // Подключаем Browser Sync
-    pug = require('gulp-jade'); // Подключение Pug
+    pug = require('gulp-jade'), // Подключение Pug
+    autoprefixer = require('gulp-autoprefixer');
     //smartgrid = require('smart-grid'); // connecting Smart grid
  
 // чтобы запустить эту задачу, наберите в командной строке gulp jade
 gulp.task('sass', function(){ // Создаем таск Sass
-    return gulp.src('app/sass/**/*.+(scss|sass)') // Берем источник
+    return gulp.src('app/sass/style.+(scss|sass)') // Берем источник
         .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
         .pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
         .pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
@@ -17,7 +18,7 @@ gulp.task('browser-sync', function() { // Создаем таск browser-sync
         server: { // Определяем параметры сервера
             baseDir: 'app' // Директория для сервера - app
         },
-        notify: false // Отключаем уведомления
+        notify: true // Отключаем уведомления
     });
 });
 gulp.task('pug', function() {
@@ -25,9 +26,18 @@ gulp.task('pug', function() {
       .pipe(pug()) 
       .pipe(gulp.dest('app')); // указываем gulp куда положить скомпилированные HTML файлы
 });
+gulp.task('default', function () {
+    return gulp.src('app/css/style.css')
+        .pipe(autoprefixer({
+            browsers: ['last 4 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('app/css/'));
+});
+
 gulp.task('watch', ['browser-sync', 'sass'], function() {
   gulp.watch('app/sass/**/*.+(scss|sass)', ['sass']); // Наблюдение за sass файлами в папке sass
-  gulp.watch('app/*.html', browserSync.reload); // Наблюдение за HTML файлами в корне проекта
+  gulp.watch('app/*.html', browserSync.reload); // Наблюдение за HTML файлами 
   gulp.watch('app/js/**/*.js', browserSync.reload); // Наблюдение за JS файлами в папке js
   gulp.watch('app/pug/*.pug', ['pug'])
 });
